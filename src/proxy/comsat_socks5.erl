2b4a74930fc69190bc0034c010169f35f12dbfcc
vans163 <vans_163@yahoo.com>
2018-10-16 16:47:27 -0400

    because some socks5 servers dont understand how tcp works and that a partial recv can happen

--- src/proxy/comsat_socks5.erl
+++ src/proxy/comsat_socks5.erl
@@ -14,21 +14,17 @@ do_server_handshake(TargetAddr, TargetPort, Socket, Username, Password, Timeout)
             ok = gen_tcp:send(Socket, <<1,(byte_size(Username)):8, Username/binary, (byte_size(Password)):8, Password/binary>>),
             {ok, <<1,0>>} = gen_tcp:recv(Socket, 2, Timeout)
     end,
-    ok = gen_tcp:send(Socket, <<5,1,0>>),
     TargetAddrType = case inet:parse_address(unicode:characters_to_list(TargetAddr)) of
         {error, einval} -> <<3>>;
         {ok, _} -> <<1>>
     end,
-    ok = gen_tcp:send(Socket, TargetAddrType),
     case TargetAddrType of
         <<1>> ->
             {ok, {IPA, IPB, IPC, IPD}} = inet:parse_address(unicode:characters_to_list(TargetAddr)),
-            ok = gen_tcp:send(Socket, <<IPA, IPB, IPC, IPD>>);
+            ok = gen_tcp:send(Socket, <<5,1,0,TargetAddrType/binary, IPA, IPB, IPC, IPD, TargetPort:16>>);
         <<3>> ->
-            ok = gen_tcp:send(Socket, <<(byte_size(TargetAddr)):8>>),
-            ok = gen_tcp:send(Socket, TargetAddr)
+            ok = gen_tcp:send(Socket, <<5,1,0,TargetAddrType/binary, (byte_size(TargetAddr)):8, TargetAddr/binary, TargetPort:16>>)
     end,
-    ok = gen_tcp:send(Socket, <<TargetPort:16>>),
     {ok, <<5,0,0,RAddrType/binary>>} = gen_tcp:recv(Socket, 4, Timeout),
     case RAddrType of
         <<1>> ->
