d67668c4081bfc67d8e90be113602796730905e5
vans163 <vans_163@yahoo.com>
2021-01-04 14:43:39 -0500

    update socks5

--- src/proxy/comsat_socks5.erl
+++ src/proxy/comsat_socks5.erl
@@ -4,16 +4,22 @@
 do_server_handshake(TargetAddr, TargetPort, Socket, Username, Password, Timeout) ->
     inet:setopts(Socket, [binary, {active, false}]),
     case Username == undefined of
-        true -> 
+        true ->
             ok = gen_tcp:send(Socket, <<5,1,0>>),
             {ok, <<5,0>>} = gen_tcp:recv(Socket, 2, Timeout);
 
-        false -> 
-            ok = gen_tcp:send(Socket, <<5,1,2>>),
-            {ok, <<5,2>>} = gen_tcp:recv(Socket, 2, Timeout),
+        false ->
+            ok = gen_tcp:send(Socket, <<5,1,2>>)
+    end,
+
+    {ok, <<5, AuthMethod>>} = gen_tcp:recv(Socket, 2, Timeout),
+    case AuthMethod of
+        0 -> ok;
+        2 ->
             ok = gen_tcp:send(Socket, <<1,(byte_size(Username)):8, Username/binary, (byte_size(Password)):8, Password/binary>>),
             {ok, <<1,0>>} = gen_tcp:recv(Socket, 2, Timeout)
     end,
+
     TargetAddrType = case inet:parse_address(unicode:characters_to_list(TargetAddr)) of
         {error, einval} -> <<3>>;
         {ok, _} -> <<1>>
@@ -31,7 +37,7 @@ do_server_handshake(TargetAddr, TargetPort, Socket, Username, Password, Timeout)
             {ok, _} = gen_tcp:recv(Socket, 4, Timeout);
         <<3>> ->
             {ok, RTarLen} = gen_tcp:recv(Socket, 1, Timeout),
-            {ok, _} = gen_tcp:recv(Socket, RTarLen, Timeout) 
+            {ok, _} = gen_tcp:recv(Socket, RTarLen, Timeout)
     end,
     {ok, _} = gen_tcp:recv(Socket, 2, Timeout),
     ok.
