5084eb3d5de4c13722e54ff1b1239dcac8eba3f0
vans163 <vans_163@yahoo.com>
2022-10-03 13:53:01 -0400

    OTP25 remove http_uri:parse

--- src/core/comsat_core_uri.erl
+++ src/core/comsat_core_uri.erl
@@ -2,40 +2,23 @@
 -compile(export_all).
 
 parse(Url) ->
-    {ok, {Scheme, Auth, Host, Port, Path, Query}} = http_uri:parse(Url, 
-        [
-            {scheme_defaults, [{http,80},{https,443},{ws,80},{wss,443}]}
-        ]),
-    Scheme2 = erlang:atom_to_binary(Scheme, unicode),
+    Uri = uri_string:parse(Url),
     
-    Auth2 = unicode:characters_to_binary(Auth),
+    Scheme = maps:get(scheme, Uri, nil),
+    UserInfo = maps:get(userinfo, Uri, nil),
+    Host = maps:get(host, Uri, nil),
+    Path = maps:get(path, Uri, nil),
+    Query = maps:get(query, Uri, nil),
 
-    Origin = io_lib:format("~p://~s:~p", [Scheme,Host,Port]),
+    Port = case Scheme of
+        <<"http">> -> 80;
+        <<"https">> -> 443;
+        <<"ws">> -> 80;
+        <<"wss">> -> 443;
+        _ -> 80
+    end,
+               
+    Origin = io_lib:format("~s://~s:~s", [Scheme,Host,Port]),
     Origin2 = unicode:characters_to_binary(Origin),
 
-    Host2 = unicode:characters_to_binary(Host),
-
-    Path2 = unicode:characters_to_binary(Path),
-
-    Query2 = unicode:characters_to_binary(Query),
-
-    %{ok,{https,[],"www.google.com",443,"/test","?te=4"}}
-
-    %{match, _Matches} = re:run(Url, 
-    %    "^(([^:\\/?#]+):)?(\\/\\/([^\/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?", 
-    %    [{capture, all_but_first, binary}]),
-    %Matches = lists:append(_Matches, [<<>>, <<>>, <<>>, <<>>, <<>>, <<>>]),
-    %Origin = <<(lists:nth(1, Matches))/binary, (lists:nth(3, Matches))/binary>>,
-    %Scheme = lists:nth(2, Matches),
-    %Host = lists:nth(4, Matches),
-    
-    %_Path = lists:nth(5, Matches),
-    %Path = case _Path of <<>> -> <<"/">>; P -> P end,
-
-    %Query = lists:nth(6, Matches),
-    %Hashbang = lists:nth(9, Matches),
-
-    %Authentication not supported
-    %nomatch = binary:match(Host, <<"@">>),
-
-    {Scheme2, Auth2, Origin2, Host2, Path2, Query2, Host2, Port}.
+    {Scheme, UserInfo, Origin2, Host, Path, Query, Host, Port}.
